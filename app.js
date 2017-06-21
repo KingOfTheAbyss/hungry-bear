fbded2d58400fac99da786b165eac05e78638ff0
KingOfTheAbyss <kaesn05@gmail.com>
2017-06-20 14:58:17 -1000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+function eatHumans(bearName, humanNames){
+	console.log(bearName);
+	console.log(humanNames);
+	console.log(bearName + " ate " + humanNames[2])
+}
+
+eatHumans("Kae-Sn", ["John Cena", "Michael Jackson", "Adam Lavine"]);
